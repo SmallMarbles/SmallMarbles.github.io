68928c74c0f822bea7bfa2351b781dd680e1b4ea
SmallMarbles <103583937+SmallMarbles@users.noreply.github.com>
2024-03-25 14:24:22 +0100

    Add files via upload

---
+++ tallyhall/script.js
@@ -0,0 +1,16 @@
+function aug2005() {
+
+// if  (window.location = "https://tallyhall.continents.repl.co/2005/") {
+// alert("You are already here! ( Press OK to continue anyways ) ")
+     window.location = "https://tallyhall.continents.repl.co/2005/"
+// }
+}
+
+function early2003() {
+//
+ // if  (window.location = "https://tallyhall.continents.repl.co/") {
+//alert("You are already here! ( Press OK to continue anyways ) ")
+     window.location = "https://tallyhall.continents.repl.co/"
+//}
+
+}
