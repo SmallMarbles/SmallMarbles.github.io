51da59ca5cde470c6449b9571ec075e7a08542c4
SmallMarbles <103583937+SmallMarbles@users.noreply.github.com>
2024-03-25 16:16:00 +0100

    Update script.js

--- tallyhall/script.js
+++ tallyhall/script.js
@@ -2,7 +2,7 @@ function aug2005() {
 
 // if  (window.location = "https://tallyhall.continents.repl.co/2005/") {
 // alert("You are already here! ( Press OK to continue anyways ) ")
-     window.location = "https://tallyhall.continents.repl.co/2005/"
+     window.location = "https://smallmarbles.github.io/tallyhall/2005/"
 // }
 }
 
@@ -10,7 +10,7 @@ function early2003() {
 //
  // if  (window.location = "https://tallyhall.continents.repl.co/") {
 //alert("You are already here! ( Press OK to continue anyways ) ")
-     window.location = "https://tallyhall.continents.repl.co/"
+     window.location = "https://smallmarbles.github.io/tallyhall/"
 //}
 
 }
